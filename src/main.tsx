f50f57aba9eba7f77c99d88d9b74afd20f4ba460
crypted <shedrack_samaila@yahoo.com>
2024-06-08 01:22:36 +0100

    ui update

--- src/main.tsx
+++ src/main.tsx
@@ -5,6 +5,7 @@ import './index.css'
 import { TonConnectUIProvider } from '@tonconnect/ui-react'
 
 const manifestUrl = "https://crypted420.github.io/dapp_with_ton_blockchain/manifest.json";
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <TonConnectUIProvider manifestUrl={manifestUrl}>
     <React.StrictMode>
